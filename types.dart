e5bf7fb399880a32fe2c66bbe8ef0f593606db91
Mahmoud-Niazy-Elsolia <mahmoudniazy44@gmail.com>
2023-09-07 22:49:51 +0300

    hungman first commit

---
+++ types.dart
@@ -0,0 +1,40 @@
+List<List<String>> types = [
+ [
+  'HORSE',
+  'LION',
+  'SNAKE',
+  'MOUSE',
+  'SHEEP',
+  'RABBIT',
+  'CAT',
+  'DOG',
+  'TIGER',
+],
+[
+  'BLACK',
+  'WHITE',
+  'RED',
+  'YELLOW',
+  'ORANGE',
+  'PINK',
+  'GREEN',
+  'BLUE',
+  'GREY'
+
+],
+ [
+  'MATH',
+  'SCIENCE',
+  'ENGLISH',
+  'ARABIC',
+   'PHYSICS',
+   'HISTORY',
+   'GEOGRAPHY',
+   'BIOLOGY'
+
+],
+];
+
+
+
+
